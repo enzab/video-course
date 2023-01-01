8a293658a69d73e83fe45d1f0d634cc2637d3149
enzab <enciksabri90@gmail.com>
2023-01-01 20:20:52 +0800

    Membuat Footer, Navbar dan Sidebar pada Backsite

---
+++ resources/views/layouts/backend/partials/navbar.blade.php
@@ -0,0 +1,35 @@
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    {{-- Left navbar links --}}
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a href="#" class="nav-link" data-widget="pushmenu" role="button">
+                <i class="fas fa-bars"></i>
+            </a>
+        </li>
+    </ul>
+
+    {{-- Right navbar links --}}
+    <ul class="navbar-nav ml-auto">
+        {{-- Notification Dropdown Menu --}}
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link" data-togg="dropdown">
+                <i class="far fa-bell">
+                    <span class="badge badge-warning navbar-badge">
+                        10
+                    </span>
+                </i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                <span class="dropdown-item dropdown-header">
+                    10 Notification
+                </span>
+                <div class="dropdown-divider"></div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="navlink" data-widget="fullscreen" role="button">
+                <i class="fas fa-expand-arrows-alt"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
